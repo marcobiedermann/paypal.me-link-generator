db1391dafdb7ae899a3a23d5792fe7d7a873b4ac
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com.>
2025-02-05 15:38:51 +0100

    feat: Form
    Add form to enter data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,110 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+/**
+ * @link https://developer.paypal.com/docs/reports/reference/paypal-supported-currencies/
+ */
+
+interface Currency {
+  name: string;
+  value: string;
+}
+
+const supportedCurrencies = [
+  { name: "Australian dollar", value: "AUD" },
+  { name: "Brazilian real", value: "BRL" },
+  { name: "Canadian dollar", value: "CAD" },
+  { name: "Chinese Renmenbi", value: "CNY" },
+  { name: "Czech koruna", value: "CZK" },
+  { name: "Danish krone", value: "DKK" },
+  { name: "Euro", value: "EUR" },
+  { name: "Hong Kong dollar", value: "HKD" },
+  { name: "Hungarian forint", value: "HUF" },
+  { name: "Israeli new shekel", value: "ILS" },
+  { name: "Japanese yen", value: "JPY" },
+  { name: "Malaysian ringgit", value: "MYR" },
+  { name: "Mexican peso", value: "MXN" },
+  { name: "New Taiwan dollar", value: "TWD" },
+  { name: "New Zealand dollar", value: "NZD" },
+  { name: "Norwegian krone", value: "NOK" },
+  { name: "Philippine peso", value: "PHP" },
+  { name: "Polish złoty", value: "PLN" },
+  { name: "Pound sterling", value: "GBP" },
+  { name: "Singapore dollar", value: "SGD" },
+  { name: "Swedish krona", value: "SEK" },
+  { name: "Swiss franc", value: "CHF" },
+  { name: "Thai baht", value: "THB" },
+  { name: "United States dollar", value: "USD" },
+] as const satisfies Currency[];
+
+const formDataSchema = z.object({
+  username: z.string(),
+  price: z.object({
+    amount: z.number().positive(),
+    currency: z.string(),
+  }),
+});
+
+type FormData = z.infer<typeof formDataSchema>;
+
 function App() {
+  const { register, handleSubmit, watch } = useForm<FormData>({
+    resolver: zodResolver(formDataSchema),
+    defaultValues: {
+      username: "biedermannmarco",
+      price: {
+        amount: 10,
+        currency: "EUR",
+      },
+    },
+  });
+
+  const {
+    username,
+    price: { amount, currency },
+  } = watch();
+  const link = `https://paypal.me/${username}/${amount}${currency}`;
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
   return (
     <div>
       <h1>PayPal.Me Link Generator</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" {...register("username")} />
+        </div>
+        <div>
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="number"
+            min="0"
+            step="0.01"
+            {...register("price.amount", {
+              min: 0,
+              valueAsNumber: true,
+            })}
+          />
+        </div>
+        <div>
+          <label htmlFor="currency">Currency</label>
+          <select {...register("price.currency")}>
+            <option></option>
+            {supportedCurrencies.map((currency) => (
+              <option key={currency.value} value={currency.value}>
+                {currency.name} ({currency.value})
+              </option>
+            ))}
+          </select>
+        </div>
+      </form>
+
+      <output>{link}</output>
     </div>
   );
 }
